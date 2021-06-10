8a7c626a58eaa3e6a1b207af9072994630d50cab
Rvoiiima <ryo@nsl.cs.waseda.ac.jp>
2021-06-10 15:46:01 +0900

    add-comment

--- matlab/make_chirpsignal.m
+++ matlab/make_chirpsignal.m
@@ -1,6 +1,9 @@
+% make chirp signal as wav file
+filename = "chirp-0-30000-60s.wav";
+
 fs = 96000;
 t = 0:1/fs:60;
 y = chirp(t,0,60,30000);
 
-filename = "chirp-0-30000-60s.wav";
+
 audiowrite(filename, y, fs);
